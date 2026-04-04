5c5eb6d2863c3354964350e96cf6d4fa2bebb8ff
thirtydracs <101961664+thirtydracs@users.noreply.github.com>
2026-04-04 15:50:15 +1100

    Update weekly.js

--- data/weekly.js
+++ data/weekly.js
@@ -1,1 +1,11 @@
-const weeklyData = [];
+const weeklyData = [
+  {
+    job: "Test Job",
+    weekEnding: "2026-04-01",
+    status: "On Track",
+    holding: "Nothing",
+    labour: "Adequate",
+    reality: "All good",
+    next: "Keep going"
+  }
+];
